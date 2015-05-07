795518bfa543e5d8a4fb38bb60144b49a4571cd1
Mohammad Reza <parsidev@users.noreply.github.com>
2015-05-07 10:33:19 +0430

    Update

--- src/Parsidev/Zarinpal/ZarinpalServiceProvider.php
+++ src/Parsidev/Zarinpal/ZarinpalServiceProvider.php
@@ -1,29 +1,28 @@
-<?php namespace Parsidev\Zaripal;
+<?php
+
+namespace Parsidev\Zarinpal;
 
 use Illuminate\Support\ServiceProvider;
 
-class ZaripalServiceProvider extends ServiceProvider {
+class ZarinpalServiceProvider extends ServiceProvider {
 
-	protected $defer = false;
-	
-	public function boot() {
+    protected $defer = false;
+
+    public function boot() {
         $this->publishes([
             __DIR__ . '/../../config/zarinpal.php' => config_path('zarinpal.php'),
         ]);
     }
 
-	public function register()
-	{
-		$this->app['zarinpal'] = $this->app->share(function($app) {
+    public function register() {
+        $this->app['zarinpal'] = $this->app->share(function($app) {
             $config = config('zarinpal');
             return new Zarinpal($config, new SoapClient($config['webServiceUrl'], array('encoding' => 'UTF-8')));
         });
-	}
+    }
 
-
-	public function provides()
-	{
-		return ['zarinpal'];
-	}
+    public function provides() {
+        return ['zarinpal'];
+    }
 
 }
